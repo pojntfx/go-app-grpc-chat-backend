d80528b09f6a614c79bd8afb9d9562994813d1d3
Felix Pojtinger <felix@pojtinger.com>
2020-09-14 23:34:48 +0200

    feat: Add full pub/sub bi-directional message streaming

--- pkg/services/chat.go
+++ pkg/services/chat.go
@@ -3,44 +3,21 @@ package services
 //go:generate sh -c "mkdir -p ../proto/generated && protoc --go_out=paths=source_relative,plugins=grpc:../proto/generated -I=../proto ../proto/*.proto"
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"sync"
-	"time"
 
-	"github.com/golang/protobuf/ptypes/empty"
 	proto "github.com/pojntfx/go-app-grpc-chat-backend/pkg/proto/generated"
+	"github.com/ugjka/messenger"
 )
 
 type ChatService struct {
 	proto.ChatServiceServer
 
-	messageChan chan *proto.ChatMessage
+	messenger *messenger.Messenger
 }
 
-func NewChatService() *ChatService {
-	return &ChatService{messageChan: make(chan *proto.ChatMessage)}
-}
-
-func (s *ChatService) CreateMessage(ctx context.Context, msg *proto.ChatMessage) (*proto.ChatMessage, error) {
-	log.Println("Received chat message", msg)
-
-	msg.Content = fmt.Sprintf("%v: %v", time.Now(), msg.Content)
-
-	s.messageChan <- msg
-
-	return msg, nil
-}
-
-func (s *ChatService) SubscribeToMessages(_ *empty.Empty, stream proto.ChatService_SubscribeToMessagesServer) error {
-	for message := range s.messageChan {
-		if err := stream.Send(message); err != nil {
-			return err
-		}
-	}
-
-	return nil
+func NewChatService(messenger *messenger.Messenger) *ChatService {
+	return &ChatService{messenger: messenger}
 }
 
 func (s *ChatService) TransceiveMessages(stream proto.ChatService_TransceiveMessagesServer) error {
@@ -51,21 +28,40 @@ func (s *ChatService) TransceiveMessages(stream proto.ChatService_TransceiveMess
 		for {
 			message, err := stream.Recv()
 			if err != nil {
-				log.Println("could not receive message from client", err)
+				log.Println("could not receive message")
 
-				return
+				break
 			}
 
-			s.messageChan <- message
+			s.messenger.Broadcast(message)
 		}
+
+		wg.Done()
 	}()
 
 	go func() {
-		for message := range s.messageChan {
-			if err := stream.Send(message); err != nil {
-				log.Println("could not send message to client", err)
+		client, err := s.messenger.Sub()
+		if err != nil {
+			log.Println("could not subscribe to messages")
+
+			s.messenger.Unsub(client)
+
+			wg.Done()
+
+			return
+		}
+
+		for message := range client {
+			if err := stream.Send(message.(*proto.ChatMessage)); err != nil {
+				log.Println("could not send message")
+
+				break
 			}
 		}
+
+		s.messenger.Unsub(client)
+
+		wg.Done()
 	}()
 
 	wg.Wait()
